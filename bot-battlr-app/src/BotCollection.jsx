9e18473f7abb30de91dc026ac2a226f67860b249
izzie <izzienjeri12@gmail.com>
2023-11-07 14:44:29 +0300

    fetched collection

--- bot-battlr-app/src/BotCollection.jsx
+++ bot-battlr-app/src/BotCollection.jsx
@@ -1,19 +1,40 @@
+import React, { useState, useEffect } from "react";
 
-import React from 'react';
-import './style.css';
+function BotCollection() {
+  const [botProfiles, setBotProfiles] = useState([]);
 
-function BotCollection({ bots, onBotClick, onEnlist }) {
+  useEffect(() => {
+    // Fetch bot profiles when the component mounts
+    fetchBotProfiles();
+  }, []);
+
+  const fetchBotProfiles = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/bots");
+      if (response.ok) {
+        const data = await response.json();
+        setBotProfiles(data);
+      } else {
+        console.error("Failed to fetch bot profiles");
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching bot profiles:", error);
+    }
+  };
+
   return (
     <div>
-      {bots.map((bot) => (
-        <div key={bot.id}>
-          <h3>{bot.name}</h3>
-          <p>Health: {bot.health}</p>
-          <p>Damage: {bot.damage}</p>
-          <button onClick={() => onBotClick(bot)}>View Details</button>
-          <button onClick={() => onEnlist(bot)}>Enlist</button>
-        </div>
-      ))}
+      <h1>Bot Profiles</h1>
+      <ul>
+        {botProfiles.map((bot) => (
+          <li key={bot.id}>
+            {/* Render bot profile information here */}
+            <p>Name: {bot.name}</p>
+            <p>Description: {bot.description}</p>
+            {/* Add more fields as needed */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
